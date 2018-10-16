84e1f85551b45059fd540bffeed793dc21064057
Jasper Li <jasli@pivotal.io>
2018-10-16 22:16:10 +0800

    update ignore file

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -x
 
+rm -f buildtmp
+mkdir buildtmp
+
 for target in linux windows darwin; do
 	for i in $(ls *.go); do
-		GOOS=$target go  build  -o ${i%".go"}_$target $i
+		GOOS=$target go  build  -o buildtmp/${i%".go"}_$target $i
 	done;
 done 
